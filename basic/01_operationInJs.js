cff1ea71ea2b5987d2c29ff500f10ad5ef7a91b3
Pradipkumar62075 <168124691+Pradipkumar62075@users.noreply.github.com>
2025-01-09 10:29:36 +0000

    detailde about datatypes

---
+++ basic/01_operationInJs.js
@@ -0,0 +1,29 @@
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(someNumber)
+console.log(typeof(stringNumber));
+
+
+// --------------------data types--------------------------
+
+// Their are main 7 data types are having in javascript
+// 7 types : String, Number, Boolean, Null, Undefinde,symbol, bigInt;
+const name = "pradip"
+const age = 33;
+const isAccout = true;
+const temperture = null;
+let userEmail;
+const id = Symbol('123')
+
+// referance Non Premitive
+// Array,Objects, Functions
+
+const Name = ["pradip", "subham", "mohan"];
+let myInfo = {
+    name:"pradip",
+    age:23
+}
+
+const myfunction = function(){
+    console.log("hello world")
+}
