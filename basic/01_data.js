f5c3210d242597e119553cffd475170cd5ad84c3
Pradipkumar62075 <168124691+Pradipkumar62075@users.noreply.github.com>
2025-01-11 05:18:21 +0000

    know about date and time

---
+++ basic/01_data.js
@@ -0,0 +1,21 @@
+// let myDate = new Date();
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+// console.log(typeof(myDate));
+
+// let myCreatedDate = new Date("11-01-2025");
+// console.log(myCreatedDate.toDateString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.getDate());
+console.log(myDate.getDay());
