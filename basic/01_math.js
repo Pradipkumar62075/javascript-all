5fe0149ec4af13899903095a54cd07b1c66540c8
Pradipkumar62075 <168124691+Pradipkumar62075@users.noreply.github.com>
2025-01-11 04:48:22 +0000

    know about math function

---
+++ basic/01_math.js
@@ -0,0 +1,15 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(6.9));
+// console.log(Math.sqrt(25));
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1 );
+console.log((Math.random()*10) +1 );
+
+min = 10;
+max = 20;
+
+console.log(Math.floor(Math.random()*(max - min +1 ))+ min);
