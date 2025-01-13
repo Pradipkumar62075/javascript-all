2b65bfa04f7183d62c9d9e1ad195c3862f104c04
Pradipkumar62075 <168124691+Pradipkumar62075@users.noreply.github.com>
2025-01-13 07:14:54 +0000

    know more about function

---
+++ 03_basics/02_function.js
@@ -0,0 +1,35 @@
+// function calculateCartPrice(num1){
+//     return num1;
+// }
+// console.log(calculateCartPrice(2));
+
+
+// function calculateCartPrice(...num1){
+//     return num1;
+// }
+// console.log(calculateCartPrice(200,300,400,500)); //it will return the whole element in a array format ;
+
+
+// function calculateCartPrice(val1,val2,...num1){
+//     return num1;
+// }
+// console.log(calculateCartPrice(200,300,400,500)); //here it will print 400 and 500 and Etc becouse starting two element will stored in val variable;
+
+
+// const user = {
+//     name: "pradip",
+//     price: 299
+// }
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`)
+
+// }
+// handleObject(user);
+
+
+const myNewArray = [100,200,300,400,500];
+function returnSecondArray(getArray){
+    return getArray;
+
+}
+console.log(returnSecondArray(myNewArray));
