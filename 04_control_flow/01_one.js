cc75f8ea7a4a53edf47826cba193f3d5a3b2236f
Pradipkumar62075 <168124691+Pradipkumar62075@users.noreply.github.com>
2025-01-18 10:21:44 +0000

    know about nulish operator and terniary operator

--- 04_control_flow/01_one.js
+++ 04_control_flow/01_one.js
@@ -56,26 +56,44 @@
 // } // if break statement no having in code that means after the match of case statement all cases are printed except default;
 
 
-const months = "may";
-switch (months) {
-    case "january":
-        console.log("january");
-        break;
-    case "february":
-        console.log("February");
-        break;
-    case "march":
-        console.log("march"); // it will print march becouse the month value is 3 that is case no.;
-        break;
-    case "april":
-        console.log("april");
-        break;
-    case "may":
-        console.log("may");
-        break;
+// const months = "may";
+// switch (months) {
+//     case "january":
+//         console.log("january");
+//         break;
+//     case "february":
+//         console.log("February");
+//         break;
+//     case "march":
+//         console.log("march"); // it will print march becouse the month value is 3 that is case no.;
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+//     case "may":
+//         console.log("may");
+//         break;
 
-    default:
-        console.log("default");
-        break;
-} // if break statement no having in code that means after the match of case statement all cases are printed except default;
+//     default:
+//         console.log("default");
+//         break;
+// } // if break statement no having in code that means after the match of case statement all cases are printed except default;
 
+
+// Nulish coalescing operator (??): null and undefined ;
+let val1 ;
+// val1 = 5 ?? 10 ; // it will print 5;
+// val1 = null ?? 10 ; // here it will print 10 becouse when the server or data base send the two value the we need to consider one where it having useful;
+// val1 = undefined ?? 15; // it will print 15
+val1 = null ?? 10 ?? 20; // it will print 10
+
+
+
+
+console.log(val1);
+
+// Terniary operator 
+
+// condition ? true : false 
+const iceTeaPrice = 100;
+iceTeaPrice >= 80 ? console.log("price is more than 80") : console.log("price is less than 80")
