60d90dc114f4efec8a58f385a309e0585aa54cee
Pradipkumar62075 <168124691+Pradipkumar62075@users.noreply.github.com>
2025-01-18 11:32:43 +0000

    know about while loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,23 @@
+// while loop
+
+// let i = 0;
+// while (i<=20){
+//     console.log(`value of i is ${i}`)
+//     // i = i + 2;
+//     i++;
+    
+// }
+
+// let myArray = ["flash","batman","superman"]
+// let i = 0;
+// while(i < myArray.length){
+//     console.log(`value of myarray is ${myArray[i]}`);
+//     i++;
+// }
+
+let Score = 1;
+do {
+    console.log(`score is ${Score}`)
+    Score ++;
+
+} while (Score <= 10);
